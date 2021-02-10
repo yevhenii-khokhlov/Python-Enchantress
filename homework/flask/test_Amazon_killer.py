59a396aae7ce527cdaf666be0b70ef5162072c97
Illia Sukonnik <illia.sukonnik@gmail.com>
2021-02-10 11:03:06 +0200

    Add practice code and homework README

--- homework/flask/test_Amazon_killer.py
+++ homework/flask/test_Amazon_killer.py
@@ -16,10 +16,29 @@ def test_create_user(store_app):
         '/users',
         json={
             "name": "Illia",
-	    "email": "illia.sukonnik@gmail.com",
-	})
+            "email": "illia.sukonnik@gmail.com",
+        })
     assert response.status_code == 201
     assert response.json == {
         "user_id": 1,
-	"registration_timestamp": '2021-02-08T14:16:41'
+        "registration_timestamp": '2021-02-08T14:16:41'
+    }
+    user_id = response.json['user_id']
+    response = store_app.get(f'/users/{user_id}')
+
+    assert response.status_code == 200
+    assert response.json == {
+        "name": "Illia",
+        "email": "illia.sukonnik@gmail.com",
+        "user_id": user_id,
+        "registration_timestamp": '2021-02-08T14:16:41',
+    }
+
+
+def test_get_user_no_such_user(store_app):
+    response = store_app.get('/users/2')
+
+    assert response.status_code == 404
+    assert response.json == {
+        "error": "no such user with id 1"
     }
